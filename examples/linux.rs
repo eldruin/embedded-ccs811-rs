15df088dcfea246b4ad7bb7a5ca57a857887c328
Diego Barrios Romero <eldruin@gmail.com>
2019-12-13 18:49:31 +0100

    Reorder code

--- examples/linux.rs
+++ examples/linux.rs
@@ -5,8 +5,8 @@ use nb::block;
 fn main() {
     let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
     let nwake = hal::Pin::new(17);
-    let address = SlaveAddr::default();
     let delay = hal::Delay {};
+    let address = SlaveAddr::default();
     let sensor = Ccs811::new(dev, address, nwake, delay);
     let mut sensor = sensor.start_application().ok().unwrap();
     loop {
