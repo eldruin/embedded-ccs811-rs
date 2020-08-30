1165cc61605ede9828bea2c04988be105d562ebf
Diego Barrios Romero <eldruin@gmail.com>
2020-08-30 10:25:10 +0200

    Use awake device and print start application error

--- examples/linux.rs
+++ examples/linux.rs
@@ -1,17 +1,21 @@
-extern crate linux_embedded_hal as hal;
-use embedded_ccs811::{prelude::*, Ccs811, MeasurementMode, SlaveAddr};
+use embedded_ccs811::{prelude::*, Ccs811Awake, MeasurementMode, ModeChangeError, SlaveAddr};
+use linux_embedded_hal::I2cdev;
 use nb::block;
 
 fn main() {
-    let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
-    let nwake = hal::Pin::new(17);
-    let delay = hal::Delay {};
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let address = SlaveAddr::default();
-    let sensor = Ccs811::new(dev, address, nwake, delay);
-    let mut sensor = sensor.start_application().ok().unwrap();
-    sensor.set_mode(MeasurementMode::ConstantPower1s).unwrap();
-    loop {
-        let data = block!(sensor.data()).unwrap();
-        println!("eCO2: {}, eTVOC: {}", data.eco2, data.etvoc);
+    let sensor = Ccs811Awake::new(dev, address);
+    match sensor.start_application() {
+        Err(ModeChangeError { dev: _, error }) => {
+            println!("Error during application start: {:?}", error);
+        }
+        Ok(mut sensor) => {
+            sensor.set_mode(MeasurementMode::ConstantPower1s).unwrap();
+            loop {
+                let data = block!(sensor.data()).unwrap();
+                println!("eCO2: {}, eTVOC: {}", data.eco2, data.etvoc);
+            }
+        }
     }
 }
